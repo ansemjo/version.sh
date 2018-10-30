bd4436b39ee1b10eb71046b03b720dc4c3985349
Anton Semjonov <anton@semjonov.de>
2018-10-30 17:03:51 +0100

    new master branch without clutter (was edca4ad)

---
+++ version.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# add 'VERSION export-subst' in .gitattributes and
+# these strings will be substituted by git-archive
+COMMIT='$Format:%H$'
+REFS='$Format:%D$'
+
+# constants
+FALLBACK_VERSION='commit'
+FALLBACK_COMMIT='unknown'
+REVISION='.r'
+
+# check if variable contains a subst value or still has the format string
+hasval() { expr match "$1" '$Format' == 0 >/dev/null; }
+
+# parse the %D reflist to get tag or branch
+refparse() { REF="$1";
+  tag=$(echo "$REF" | sed -n 's/.*tag: \([^,]*\).*/\1/p'); test -n "$tag" && echo "$tag" && return 0;
+  branch=$(echo "$REF" | sed -n 's/.* -> \([^,]*\).*/\1/p'); test -n "$branch" && echo "$branch" && return 0;
+  return 1; }
+
+# git functions to return commit and version in repository
+hasgit() { test -d .git; }
+gitcommit() { hasgit && git describe --always --abbrev=0 --exclude=\* --dirty; }
+gitversion() { hasgit \
+  && { V=$(git describe 2>/dev/null) && echo "$V" | sed "s/-\([0-9]*\)-g.*/$REVISION\1/"; } \
+  || { C=$(git rev-list --count HEAD) && printf '0.0.0%s%s' "$REVISION" "$C"; };
+}
+
+# wrappers
+version() { hasval "$REFS" && refparse "$REFS" || gitversion || echo "$FALLBACK_VERSION"; }
+commit()  { hasval "$COMMIT" && echo "$COMMIT" || gitcommit || echo "$FALLBACK_COMMIT";  }
+
+# ---------------------------------
+
+case "$1" in
+  version) version ;;
+  commit)  commit ;;
+  describe)
+    printf '%s-g%.7s\n' "$(version)" "$(commit)" ;;
+  help)
+    printf '%s [version|commit|describe]\n' "$0" ;;
+  *)
+    printf 'version : %s\ncommit  : %s\n' "$(version)" "$(commit)" ;;
+esac
